d262cecfb46b04cd692de7ca5b80f7f563801963
sivakotireddy45 <sasibrucelee1@gmail.com>
2024-07-09 10:56:11 +0530

    Objects and classes

--- app/src/main/java/com/example/trail_1/demo.kt
+++ app/src/main/java/com/example/trail_1/demo.kt
@@ -1,10 +1,14 @@
 package com.example.trail_1
 
+fun main() {
 class demo {
+    val s = Square(10)
+    for (i in 1..5) print(i)
+
     printHello(name = "hello")
     drive(speed = "slow")
     tempToday("mon",32)
-    var name:String = "Siva"
+    var name: String = "Siva"
     println(name.commaSeperated("some other string"))
 }
 
@@ -14,8 +18,16 @@ fun printHello(name: String?){
             println("Today is $day and it's $temp degrees.")
         }
 
-        fun String.commaSeperated( values:String):String{
-            return  "comma seperated string --"+ values
+        fun String.commaSeperated( values:String):String {
+            return "comma seperated string --" + values
 
         }
+
+        }
+}}
+
+class Square(val side: Int) {
+    init {
+        println(side * 2)
+    }
 }
